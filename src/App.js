de024706b685227251eeb7f6f308c9921289fabf
Raghuvansh Sharma <raghuvansharma24@gmail.com>
2026-09-10 12:09:24 +0530

    Update API key in App.js
    Replaced API key with a hardcoded value.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './index.css';
 
 const App = () => {
   const pageSize = 9;
-  const apiKey = process.env.REACT_APP_NEWS_API;
+  const apiKey = af487af494e16fe970c491ca8bc7923f;
   const [progress, setProgress] = useState(0);
 
   const newsProps = (category) => ({
